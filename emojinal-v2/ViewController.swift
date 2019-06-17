083054d2785ba419db4d34fbd005b18261d02615
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-17 00:25:36 -0500

    Initial Commit

---
+++ emojinal-v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emojinal-v2
+//
+//  Created by Ashanti Williams on 6/17/19.
+//  Copyright © 2019 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ emojinal-v2Tests/emojinal_v2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  emojinal_v2Tests.swift
+//  emojinal-v2Tests
+//
+//  Created by Ashanti Williams on 6/17/19.
+//  Copyright © 2019 Ashanti Williams. All rights reserved.
+//
+
+import XCTest
+@testable import emojinal_v2
+
+class emojinal_v2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
