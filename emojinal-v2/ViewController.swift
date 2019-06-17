49540af202c75447d8f1731a396dad4f6e376bc7
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-17 08:59:06 -0500

    Complete Iteration 2. Includes multiple options on button press.

--- emojinal-v2/ViewController.swift
+++ emojinal-v2/ViewController.swift
@@ -5,27 +5,27 @@
 //  Created by Ashanti Williams on 6/17/19.
 //  Copyright © 2019 Ashanti Williams. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let emojis = ["😂":"laugh out loud", "🙃":"welp", "🤠":"cowboy", "🤓":"nerdy fun times", "🥰":"feelin' myself"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func showMessage(sender: UIButton) {
         
+        let selectedEmotion = sender.titleLabel!.text
         //create the alert
-        let tojAlert =  UIAlertController (title: "I'm so emojinal!!", message: "Like really emojinal", preferredStyle: UIAlertController.Style.alert)
+        let emojiAlert =  UIAlertController (title: "You're so emojinal! \n Here's a song!", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
         //adding the buttons
-        tojAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
-        
+        emojiAlert.addAction(UIAlertAction(title: "OMG YASSSS!!!", style: UIAlertAction.Style.default, handler: nil))
+        emojiAlert.addAction(UIAlertAction(title: "Thanks!", style: UIAlertAction.Style.default, handler: nil))
+        emojiAlert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.default, handler: nil))
         //show the alert
-        present(tojAlert, animated: true, completion: nil)
-    
+        present(emojiAlert, animated: true, completion: nil)
     }
-    
-    
 }
+
 
