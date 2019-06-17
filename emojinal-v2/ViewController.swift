7d6d59d0c23e16c5ac847024f2f69e4a795d6cdb
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-17 07:39:16 -0500

    Complete Iteration 1.

--- emojinal-v2/ViewController.swift
+++ emojinal-v2/ViewController.swift
@@ -14,7 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showMessage(sender: UIButton) {
+        //create the alert
+        let tojAlert =  UIAlertController (title: "Wanna hear a joke?", message: "Time flies like an arrow, fruit flies like banana.", preferredStyle: UIAlertController.Style.alert)
+        //adding the buttons
+        tojAlert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+        
+        //show the alert
+        present(tojAlert, animated: true, completion: nil)
+    
+    }
+    
+    
 }
 
